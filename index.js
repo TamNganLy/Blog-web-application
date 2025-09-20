77023e60c21599de37fdae4cd0cbbbe74c4c9c48
TamNganLy <tamly1810@gmail.com>
2025-09-19 20:08:59 -0700

    completed styling web version

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import fs from "fs";
 import multer from "multer";
 import path from "path";
+import { title } from "process";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -34,13 +35,26 @@ app.get("/create", (req, res) => {
 app.get("/edit/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const editPost = data.find(post => post.id === id);
+  let title = ""; 
   let quote = "";
+  let datePost = editPost.datePost; 
+
+  datePost = datePost.replace(/^Posted on\s*/, '');
+
+  const dateObj = new Date(datePost);
+
+  const formattedForInput = dateObj.toISOString().split('T')[0];
+
   if (editPost) {
+    title = editPost.title;
+    datePost = formattedForInput;
     quote = editPost.quote.replace(/<br\s*\/?>/g, '\n');
   };
 
   res.render("create-edit.ejs", {
     id,
+    title,
+    datePost,
     quote
   });
 });
@@ -57,8 +71,22 @@ app.post("/upload", upload.single("video"), (req, res) => {
 
   const formattedQuote = req.body.quote.trim().replace(/\r?\n/g, '<br />');
 
+  const inputDate = req.body.datePost;
+
+  const dateObj = new Date(inputDate);
+
+  const formattedDate = dateObj.toLocaleDateString('en-US', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    timeZone: 'UTC'
+  });
+
   const newPost = {
     id,
+    title: req.body.title.trim(),
+    datePost: `Posted on ${formattedDate}`,
     video: `/videos/${videoName}`,
     quote: formattedQuote
   };
@@ -66,7 +94,6 @@ app.post("/upload", upload.single("video"), (req, res) => {
   data.push(newPost);
   fs.writeFileSync("./data/data.json", JSON.stringify(data, null, 2));
 
-  console.log(`Saved post ${id}`);
   res.redirect("/");
 });
 
@@ -81,12 +108,24 @@ app.post("/upload/:id", upload.single("video"), (req, res) => {
 
   const post = data[postIndex];
 
+  const inputDate = req.body.datePost;
+
+  const dateObj = new Date(inputDate);
+
+  const formattedDate = dateObj.toLocaleDateString('en-US', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    timeZone: 'UTC'
+  });
+
   const formattedQuote = req.body.quote.trim().replace(/\r?\n/g, '<br />');
 
-  // Update quote
-  if (req.body.quote) {
-    post.quote = formattedQuote;
-  }
+  // Update post
+  post.title = req.body.title.trim();
+  post.datePost = `Posted on ${formattedDate}`;
+  post.quote = formattedQuote;
 
   // If new video uploaded, replace old one
   if (req.file) {
